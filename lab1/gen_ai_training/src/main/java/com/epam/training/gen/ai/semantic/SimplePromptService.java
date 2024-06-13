c0e145ff0ffde36858ec18bf2333643b80318ff0
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-06-13 12:00:46 +0300

    Refactor SimplePromptService class
    The refactoring of SimplePromptService class includes changes to dependencies and method improvement. The OpenAIAsyncClientService class has been replaced with OpenAIAsyncClient and the code for the getChatCompletions method has been rewritten in a more readable and concise form.

--- lab1/gen_ai_training/src/main/java/com/epam/training/gen/ai/semantic/SimplePromptService.java
+++ lab1/gen_ai_training/src/main/java/com/epam/training/gen/ai/semantic/SimplePromptService.java
@@ -1,45 +1,43 @@
 package com.epam.training.gen.ai.semantic;
 
+import com.azure.ai.openai.OpenAIAsyncClient;
+import com.azure.ai.openai.models.ChatChoice;
 import com.azure.ai.openai.models.ChatCompletions;
 import com.azure.ai.openai.models.ChatCompletionsOptions;
 import com.azure.ai.openai.models.ChatRequestUserMessage;
-import com.epam.training.gen.ai.semantic.client.OpenAIAsyncClientService;
+import com.azure.ai.openai.models.ChatResponseMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @Slf4j
 public class SimplePromptService {
-
     public static final String GREETING_MESSAGE = "Say hi";
 
-    private final OpenAIAsyncClientService aiClientService;
-    private final String deploymentOrModelName;
+    private final OpenAIAsyncClient aiClientService;
+    @Value("${client-azureopenai-deployment-name}")
+    private String deploymentOrModelName;
 
-    public SimplePromptService(OpenAIAsyncClientService aiClientService,
-                               @Value("${client-azureopenai-deployment-name}")  String deploymentOrModelName) {
+    public SimplePromptService(OpenAIAsyncClient aiClientService) {
         this.aiClientService = aiClientService;
-        this.deploymentOrModelName = deploymentOrModelName;
     }
 
     public List<String> getChatCompletions() {
-
-        ChatCompletions completions = aiClientService.get()
-                .getChatCompletions(
-                        deploymentOrModelName,
-                        new ChatCompletionsOptions(
-                                List.of(new ChatRequestUserMessage(GREETING_MESSAGE))))
+        var chatCompletionsOptions = new ChatCompletionsOptions(List.of(new ChatRequestUserMessage(GREETING_MESSAGE)));
+        var completions = aiClientService
+                .getChatCompletions(deploymentOrModelName, chatCompletionsOptions)
                 .block();
-        List<String> messages = completions.getChoices().stream()
-                .map(c -> c.getMessage().getContent())
-                .collect(Collectors.toList());
+        var messages = Stream.ofNullable(completions)
+                .map(ChatCompletions::getChoices)
+                .flatMap(List::stream)
+                .map(ChatChoice::getMessage)
+                .map(ChatResponseMessage::getContent)
+                .toList();
         log.info(messages.toString());
         return messages;
     }
 }
-
-
