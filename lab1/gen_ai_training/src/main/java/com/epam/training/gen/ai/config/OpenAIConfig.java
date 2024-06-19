e1f3ae817a676c14f9e7498cc50d8fcf36fc151d
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-06-19 19:04:44 +0300

    Add PromptExecutionSettings and ChatHistory Beans
    The update introduces two new beans to the OpenAIConfig.java file. This includes creating a method to initialize and return an instance of PromptExecutionSettings, and another one to initialize and return a ChatHistory instance. These configurations will aid in executing prompts and managing chat history respectively.

--- lab1/gen_ai_training/src/main/java/com/epam/training/gen/ai/config/OpenAIConfig.java
+++ lab1/gen_ai_training/src/main/java/com/epam/training/gen/ai/config/OpenAIConfig.java
@@ -5,7 +5,9 @@ import com.azure.ai.openai.OpenAIClientBuilder;
 import com.azure.core.credential.AzureKeyCredential;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.orchestration.PromptExecutionSettings;
 import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
+import com.microsoft.semantickernel.services.chatcompletion.ChatHistory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -42,6 +44,19 @@ public class OpenAIConfig {
         return Kernel.builder()
                 .withAIService(ChatCompletionService.class, chatCompletionService)
                 .build();
+    }
+
+    @Bean
+    public PromptExecutionSettings promptExecutionSettings() {
+        return PromptExecutionSettings.builder()
+                .withMaxTokens(1_000)
+                .withTemperature(0.0)
+                .build();
+    }
+
+    @Bean
+    public ChatHistory chatHistory() {
+        return new ChatHistory();
     }
 
     @Bean
