e0f06d66238db0aa664e74a5d84ef7d3fbc9114d
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-06-26 11:07:48 +0300

    Remove unused AI services and simplify configuration
    Optimized the AI training application by deleting two unused services: `FunctionMessagePromptService.java` and `SinglePromptService.java`. Additionally, removed unnecessary post-construct method for splitting deployment names in `DeploymentConfig.java`, now deployment names are split directly at variable assignment.

--- lab1/gen_ai_training/src/main/java/com/epam/training/gen/ai/config/DeploymentConfig.java
+++ lab1/gen_ai_training/src/main/java/com/epam/training/gen/ai/config/DeploymentConfig.java
@@ -1,24 +1,15 @@
 package com.epam.training.gen.ai.config;
 
-import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
 import java.util.List;
 
+@Getter
 @Configuration
 public class DeploymentConfig {
 
-    @Value("${client-azureopenai-valid-deployment-names}")
-    private String validDeploymentNames;
-
-    @Getter
+    @Value("#{'${client-azureopenai-valid-deployment-names}'.split(',')}")
     private List<String> validDeploymentNamesList;
-
-    @PostConstruct
-    public void init() {
-        validDeploymentNamesList = Arrays.asList(validDeploymentNames.split(","));
-    }
 }
