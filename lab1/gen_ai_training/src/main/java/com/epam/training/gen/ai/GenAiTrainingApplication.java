a92c87ae041ba7f2d00e148966795e70f5b0bf2f
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-06-26 08:43:09 +0300

    Enable Feign clients and add property source in application
    This modification allows the application to use Feign for HTTP requests. In addition, it includes an external file ('deployment-names.properties') as a source of property values for the application context, providing an easy way to configure runtime settings.

--- lab1/gen_ai_training/src/main/java/com/epam/training/gen/ai/GenAiTrainingApplication.java
+++ lab1/gen_ai_training/src/main/java/com/epam/training/gen/ai/GenAiTrainingApplication.java
@@ -2,8 +2,12 @@ package com.epam.training.gen.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableFeignClients
+@PropertySource("classpath:config/deployment-names.properties")
 public class GenAiTrainingApplication {
 
     public static void main(String[] args) {
