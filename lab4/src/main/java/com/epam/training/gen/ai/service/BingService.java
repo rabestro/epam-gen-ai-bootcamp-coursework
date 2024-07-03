626ed2a8b392b1e23eb67709ed69b5eace887fa7
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-07-03 15:05:19 +0300

    add BingService

---
+++ lab4/src/main/java/com/epam/training/gen/ai/service/BingService.java
@@ -0,0 +1,32 @@
+package com.epam.training.gen.ai.service;
+
+import com.azure.ai.openai.OpenAIAsyncClient;
+import com.epam.training.gen.ai.semantic.SemanticKernelBasicService;
+import com.epam.training.gen.ai.semantic.plugin.BingSearchPlugin;
+import com.microsoft.semantickernel.services.ServiceNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BingService extends SemanticKernelBasicService {
+
+    public BingService(
+            OpenAIAsyncClient client,
+            @Value("${client-azureopenai-deployment-name}") String modelId
+    ) throws ServiceNotFoundException {
+        super(client, modelId);
+    }
+
+    @Override
+    protected Object getPlugin() {
+        return new BingSearchPlugin();
+    }
+
+    @Override
+    protected String getSystemPrompt() {
+        return """
+                You will be given a search query.
+                Your task is to create a URL for given search query at www.bing.com.
+                """;
+    }
+}
