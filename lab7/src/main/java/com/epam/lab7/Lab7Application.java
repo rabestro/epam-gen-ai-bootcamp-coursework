2cb5105d413959a08c77e54473e5e290f073442b
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-07-27 13:54:23 +0300

    chore: Update Lab7Application with chat client and chat bean

--- lab7/src/main/java/com/epam/lab7/Lab7Application.java
+++ lab7/src/main/java/com/epam/lab7/Lab7Application.java
@@ -1,13 +1,25 @@
 package com.epam.lab7;
 
+import com.epam.lab7.documentreader.Chat;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Lab7Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Lab7Application.class, args);
     }
 
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+        return chatClientBuilder.build();
+    }
+
+    @Bean
+    public Chat chat(ChatClient chatClient, VectorStore vectorStore) {
+        return new Chat(chatClient, vectorStore);
+    }
 }
