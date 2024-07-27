6f3de536a88160f8558a24cb91b63c7c69b30ecf
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-07-27 13:00:13 +0300

    chore: Add TreasureController for handling treasure-related requests

---
+++ lab7/src/main/java/com/epam/lab7/v1/controller/TreasureController.java
@@ -0,0 +1,32 @@
+package com.epam.lab7.v1.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TreasureController {
+    private final ChatClient chatClient;
+
+    /**
+     * The system message can be used to specify the persona used by the model in its replies.
+     */
+    public TreasureController(ChatClient.Builder builder) {
+        this.chatClient = builder
+                .defaultSystem("Please respond to any question in the voice of a pirate.")
+                .build();
+    }
+
+    /**
+     * Respond to inquiries about pirate treasure
+     */
+    @GetMapping("/treasure")
+    public String treasureFacts() {
+        return chatClient.prompt()
+                .user("Tell me a really interesting fact about famous pirate treasures. Please keep your answer to 1 or 2 sentences.")
+                .call()
+                .content();
+    }
+}
